524424155cc476ccdf4319872c082ce0db88e496
Artem Bragin <sir.temab@gmail.com>
2025-04-01 16:24:47 +0700

    firstCommit

---
+++ mindGames/mindGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  mindGamesApp.swift
+//  mindGames
+//
+//  Created by Артём Брагин on 01.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct mindGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
